7f26b1a7d3c51f414400c50c709e77d44a54540d
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-27 03:06:32 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,10 @@
-counter = 0 
-while counter < 10 
-puts "Wingardium Leviosa"
-end  
+def using_while 
+  levitation_force = 6
+while levitation_force < 10
+  puts "Wingardium Leviosa"
+  levitation_force += 1
+end 
+end 
 
-counter = 0 
-until counter < 10 
-puts "Wingardium Leviosa"
-end  
+
+
